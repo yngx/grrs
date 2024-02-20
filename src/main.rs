30d16a987acb10bbe5625f63eb6e738d759442e5
Xao <divxyng.edu@gmail.com>
2024-02-19 23:20:38 -0800

    Read entire file to memory

--- src/main.rs
+++ src/main.rs
@@ -12,5 +12,11 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
 }
